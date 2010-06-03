29d579f8d83b61b33186d8a24703910b273691a4
Glenn Siegman <gsiegman@gsiegman.com>
2010-06-03 02:05:58 -0500

    places to venues

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@
 
 from distutils.core import setup
 
-setup(name='django-places',
+setup(name='django-venues',
         version='0.1',
-        description='A django app to manage and work with locations & places.',
+        description='A django app to manage and work with locations & venues.',
         author='Glenn Siegman',
         author_email='gsiegman@gsiegman.com',
-        url='http://github.com/gsiegman/django-places',
-        packages=['places']
+        url='http://github.com/gsiegman/django-venues',
+        packages=['venues']
 )
