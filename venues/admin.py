45e73d245da0b2cba39fb3932d5930243cc55725
Glenn Siegman <gsiegman@gsiegman.com>
2010-06-05 14:36:01 -0500

    added venue image admin

--- venues/admin.py
+++ venues/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from venues.models import Location, Venue
+from venues.models import Location, Venue, VenueImage
 
 class VenueAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug': ('name',)}
 
 admin.site.register(Venue, VenueAdmin)
 admin.site.register(Location)
+admin.site.register(VenueImage)
