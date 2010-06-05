cd236922259b9a10f6a906b213f454d4cdcb2ce9
Glenn Siegman <gsiegman@gsiegman.com>
2010-06-05 14:21:36 -0500

    added imagekit req and venue image

--- venues/models.py
+++ venues/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.localflavor.us.models import USStateField, PhoneNumberField
+from imagekit.models import ImageModel
 
 class Location(models.Model):
     """
@@ -20,13 +21,27 @@ class Location(models.Model):
     def full_address(self):
         return '%s %s, %s %s' % (self.address, self.city, 
                                  self.state, self.zip_code)
-    
+
+class VenueImage(ImageModel):
+    """
+    A venue's image
+    """
+    original_image = models.ImageField(upload_to='venue_images')
+    num_views = models.PositiveIntegerField(editable=False, default=0)
+
+    class IKOptions:
+        spec_module = 'venues.specs'
+        cache_dir = 'venue_images'
+        image_field = 'original_image'
+        save_count_as = 'num_views'
+        
 class Venue(models.Model):
     """
     A place where something occurs.
     """
     name = models.CharField(max_length=255)
     slug = models.SlugField(unique=True)
+    image = models.ForeignKey(VenueImage, blank=True, null=True)
     description = models.TextField()
     phone = PhoneNumberField(blank=True)
     website = models.URLField(blank=True)
